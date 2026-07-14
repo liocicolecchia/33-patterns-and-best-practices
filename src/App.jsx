7797c431a3ef9592c7d07daa07e9b336fb097d83
Lio <liocicolecchia@gmail.com>
2026-07-15 00:11:40 +0200

    introduction to compound components. starting to build an accordeon where multiple components relie on each other. Adding an accordion and AccordionItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import Accordion from "./components/Accordion";
+import AccordionItem from "./components/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+
+        <Accordion className="accordion">
+          <AccordionItem className="accordion-item" title="We got 20 years of experience">
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the business of planing vacation trips for more then 20 years.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem title="We are working with local guides">
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the business of planing vacation trips for more then 20 years.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,5 @@
+function Accordion({ children, className }) {
+  return <ul className={className}>{children}</ul>;
+}
+
+export default Accordion;
