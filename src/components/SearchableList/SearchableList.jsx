c3d875e6e192f58579fb5e5e2b34630945cb3da8
Lio <liocicolecchia@gmail.com>
2026-07-15 20:09:59 +0200

    adding search logic to the component

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function SearchableList({ items }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResult = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Search" />
+      <input type="search" placeholder="Search" onChange={handleChange} />
       <ul>
-        {items.map((item, index) => (
+        {searchResult.map((item, index) => (
           <li key={index}>{item.toString()}</li>
         ))}
       </ul>
